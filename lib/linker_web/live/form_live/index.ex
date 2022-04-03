ad6e760a6cd86ca67018464a35d08a91bd6a8b2f
feliciofilipe <filipe.felicio.1999@gmail.com>
2022-04-03 22:33:22 +0100

    Improve table

--- lib/linker_web/live/form_live/index.ex
+++ lib/linker_web/live/form_live/index.ex
@@ -11,7 +11,10 @@ defmodule LinkerWeb.FormLive.Index do
 
   @impl true
   def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+    {:noreply,
+      socket
+      |> assign(:current_page, :forms)
+      |> apply_action(socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
--- lib/linker_web/live/link_live/index.ex
+++ lib/linker_web/live/link_live/index.ex
@@ -11,7 +11,10 @@ defmodule LinkerWeb.LinkLive.Index do
 
   @impl true
   def handle_params(params, _url, socket) do
-    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+    {:noreply,
+      socket
+      |> assign(:current_page, :links)
+      |> apply_action(socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
