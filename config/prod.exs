ffe6b7de5c2d89478343f49ed0a93871e572aa5c
Nelson Estevão <nelsonmestevao@gmail.com>
2022-05-01 17:57:45 +0100

    Fix URL deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :linker, LinkerWeb.Endpoint,
-  url: [scheme: "https", host: "join-linker.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "joinum.link", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
