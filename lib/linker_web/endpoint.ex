0862966b3129d61e52c210dd5aba270be4475309
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-06-16 14:56:56 +0000

    Add favicon (#31)
    * Add favicon
    
    * Change title
    
    Co-authored-by: Nelson Estevão <nelsonmestevao@proton.me>

--- lib/linker_web/endpoint.ex
+++ lib/linker_web/endpoint.ex
@@ -21,7 +21,7 @@ defmodule LinkerWeb.Endpoint do
     at: "/",
     from: :linker,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(assets fonts images favicon favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
